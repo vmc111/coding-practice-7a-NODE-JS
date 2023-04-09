15ec44e33bc40d187b9732e77bf13c173f02528c
vmc111 <vamship625@gmail.com>
2023-04-09 03:54:01 +0000

    API 4

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 
 const app = express();
+app.use(express.json());
 const dbPath = path.join(__dirname, "cricketMatchDetails.db");
 
 // INITIALIZE DATA BASE
@@ -47,6 +48,42 @@ app.get("/players/:playerId/", async (request, response) => {
     WHERE player_id = ${playerId};`;
   let player = await db.get(getPlayerQuery);
   response.send(player);
+});
+
+// API 3
+
+app.put("/players/:playerId/", async (request, response) => {
+  const { playerId } = request.params;
+  const { playerName } = request.body;
+  console.log(request.body);
+
+  const putQuery = `
+      UPDATE
+          player_details
+      SET
+          player_name = '${playerName}'
+      WHERE
+          player_id = ${playerId};`;
+
+  await db.run(putQuery);
+  response.send("Player Details Updated");
+});
+
+// API 4
+
+app.get("/matches/:matchId/", async (request, response) => {
+  const { matchId } = request.params;
+
+  const getMatchDetailsQuery = `
+    SELECT 
+        match_id AS matchId, 
+        match, 
+        year
+    FROM match_details 
+    WHERE match_id = ${matchId};`;
+
+  let matchDetails = await db.get(getMatchDetailsQuery);
+  response.send(matchDetails);
 });
 
 module.exports = app;
